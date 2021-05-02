c99a3bd079d40f873f6599138fe65e5cf758db48
yaojie <limyaojie93@gmail.com>
2021-05-02 22:17:28 +0800

    fix posts index props

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -4,17 +4,17 @@ import Layout from '../../components/layout'
 import { formatDate } from '../../lib/date'
 import { getSortedPostsData, PostData } from '../../lib/posts'
 
-export async function getStaticProps(): Promise<GetStaticPropsResult<PostData[]>> {
-  const allPostsData = getSortedPostsData()
+export async function getStaticProps(): Promise<GetStaticPropsResult<{ posts: PostData[] }>> {
+  const posts = getSortedPostsData()
   return {
-    props: allPostsData
+    props: { posts }
   }
 }
 
-export default function Posts(data: PostData[]): JSX.Element {
+export default function Posts({ posts }: { posts: PostData[] }): JSX.Element {
   return <Layout>
     <ul>
-      {data.map(({ id, date, title }) => (
+      {posts.map(({ id, date, title }) => (
         <li key={id} className='mb-4'>
           <Link href={`/posts/${id}`}>
             <a>
